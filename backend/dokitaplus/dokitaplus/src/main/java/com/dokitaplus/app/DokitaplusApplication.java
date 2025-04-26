e0ea6f6d3b493e27e69a52ae247ff2255a3179da
uche2064 <godswilllek02@gmail.com>
2025-04-26 22:03:29 +0000

    initilisation du projet spring

---
+++ backend/dokitaplus/dokitaplus/src/main/java/com/dokitaplus/app/DokitaplusApplication.java
@@ -0,0 +1,13 @@
+package com.dokitaplus.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DokitaplusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DokitaplusApplication.class, args);
+	}
+
+}
